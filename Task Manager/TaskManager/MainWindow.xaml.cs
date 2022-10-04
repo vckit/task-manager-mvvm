d7a5519e88e93924ffd3cf742265979b83fe0afc
Abdulkhakim Magomedov <salimsoltanovich@gmail.com>
2022-10-04 14:44:52 +0300

    task manager v.1

---
+++ Task Manager/TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TaskManager
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Priorities.ItemsSource = Enum.GetValues(typeof(ProcessPriorityClass)).Cast<ProcessPriorityClass>();
+            Priorities.SelectedIndex = 0;
+        }
+
+        private void TasksList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+
+            if (listBox.SelectedItems.Count > 0)
+            {
+                var viewModel = (ViewModel)DataContext;
+                viewModel.SelectedProcess = ((ProcessListItem)listBox.SelectedItems[0]).Process;
+            }
+        }
+
+        private void RefreshButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (ViewModel)DataContext;
+            viewModel.UpdateProcesses(sender, e);
+        }
+
+        private void ChangePriority_OnClick(object sender, RoutedEventArgs e)
+        {
+            var priority = (ProcessPriorityClass)Priorities.SelectionBoxItem;
+            var viewModel = (ViewModel)DataContext;
+            viewModel.ChangePriority(priority);
+        }
+
+        private void KillProcess_OnClick(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (ViewModel)DataContext;
+            viewModel.KillSelectedProcess();
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            var processListItem = (ProcessListItem)checkBox.DataContext;
+            processListItem.KeepAlive = true;
+        }
+
+        private void ToggleButton_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            var processListItem = (ProcessListItem)checkBox.DataContext;
+            processListItem.KeepAlive = false;
+        }
+    }
+}
